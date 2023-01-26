14b0fee62261f0817d315c873549c6eea9c5a477
Daniel Kjellid <daniel.kjellid@gmail.com>
2023-01-26 13:02:34 +0100

    Start from a scratch

--- nest/config.py
+++ nest/config.py
@@ -1,11 +1,48 @@
-from pydantic import BaseSettings
+import logging
+import os
 
+from starlette.config import Config
+from starlette.datastructures import Secret
 
-class Settings(BaseSettings):
-    database_url: str
+log = logging.getLogger(__name__)
 
-    class Config:
-        env_file = "../.env"
+config = Config("../.env")
 
+LOG_LEVEL = config("LOG_LEVEL", default=logging.WARNING)
+ENVIRONMENT = config("ENVIRONMENT", default="local")
 
-settings = Settings()
+##################
+# Authentication #
+##################
+
+NEST_ENCRYPTION_KEY = config("NEST_ENCRYPTION_KEY", cast=Secret, default="supersecret")
+NEST_JWT_AUDIENCE = config("NEST_JWT_AUDIENCE", default=None)
+NEST_JWT_SECRET_KEY = config("NEST_JWT_SECRET_KEY", default=None)
+NEST_JWT_ALG = config("NEST_JWT_ALG", default="HS256")
+NEST_JWT_EXP = config("NEST_JWT_EXP", cast=int, default=14 * 24 * 60 * 60)
+
+##########
+# Sentry #
+##########
+
+SENTRY_ENABLED = config("SENTRY_ENABLED", default="")
+SENTRY_DSN = config("SENTRY_DSN", default="")
+
+############
+# Database #
+############
+
+DATABASE_URL = config("DATABASE_URL", default="nest:nest@localhost:5438/nest")
+
+###########
+# Alembic #
+###########
+
+ALEMBIC_REVISION_PATH = config(
+    "ALEMBIC_REVISION_PATH",
+    default=f"{os.path.dirname(os.path.realpath(__file__))}/database/revisions/",
+)
+ALEMBIC_INI_PATH = config(
+    "ALEMBIC_INI_PATH",
+    default=f"{os.path.dirname(os.path.realpath(__file__))}/alembic.ini",
+)
