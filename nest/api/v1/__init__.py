e70845f90465cbaa20f6243e3b1cd32874c81049
Daniel Kjellid <daniel.kjellid@gmail.com>
2023-01-26 15:06:32 +0100

    Add test endpoint code

--- nest/api/v1/__init__.py
+++ nest/api/v1/__init__.py
@@ -1,8 +1,14 @@
 from fastapi import APIRouter
-from nest.api.v1.users import user_router
 
-v1_router = APIRouter(prefix="v1/")
-v1_router.include_router(user_router, prefix="users/", tags=["Users"])
+# from .users import user_router
+from .users import users_router
 
+# def init_v1_routers(router: APIRouter) -> list[APIRouter]:
+#     return [user_router]
+#     # router.include_router(user_router, prefix="/users", tags=["Users"])
+
+
+v1_router = APIRouter(prefix="/v1")
+v1_router.include_router(users_router, prefix="/users", tags=["Users"])
 
 __all__ = ["v1_router"]
--- nest/api/v1/users/__init__.py
+++
@@ -1,5 +0,0 @@
-from fastapi import APIRouter
-
-user_router = APIRouter()
-
-__all__ = ["user_router"]
