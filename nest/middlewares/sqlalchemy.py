9f3f559a5b8552c06cff00cb323d3d58cca0e9f0
Daniel Kjellid <daniel.kjellid@gmail.com>
2023-01-26 14:15:48 +0100

    Add middleware code

---
+++ nest/middlewares/sqlalchemy.py
@@ -0,0 +1,26 @@
+from uuid import uuid4
+
+from nest.database import session
+from nest.database.utils import reset_session_context, set_session_context
+from starlette.types import ASGIApp, Receive, Scope, Send
+
+
+class SQLAlchemyMiddleware:
+    """
+    Middle to initialize async database session.
+    """
+
+    def __init__(self, app: ASGIApp) -> None:
+        self.app = app
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        session_id = str(uuid4())
+        context = set_session_context(session_id=session_id)
+
+        try:
+            await self.app(scope, receive, send)
+        except Exception as exc:
+            raise exc
+        finally:
+            await session.remove()
+            reset_session_context(context=context)
