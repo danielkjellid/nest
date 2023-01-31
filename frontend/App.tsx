72efadbb4563f0c41b835c5c47e548bf2e1c3f4f
Daniel Kjellid <daniel.kjellid@gmail.com>
2023-01-31 15:27:38 +0100

    Add antd

---
+++ frontend/App.tsx
@@ -0,0 +1,49 @@
+import { Layout, Menu, theme } from "antd";
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import React, { useState } from "react";
+
+const { Header, Footer, Sider, Content } = Layout;
+
+function App() {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <Layout className="min-h-screen">
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="h-16 bg-gray-200">Logo</div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          items={[
+            { key: 1, icon: <UploadOutlined />, label: "nav 1" },
+            { key: 2, icon: <UploadOutlined />, label: "nav 2" },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ background: colorBgContainer }}>
+          {React.createElement(
+            collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+            {
+              className: "trigger",
+              onClick: () => setCollapsed(!collapsed),
+            }
+          )}
+        </Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </Layout>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./frontend/public/index.html", "./frontend/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
