8dce3ebbddbc76fe1a3bee3980e60906d9681204
Daniel Kjellid <daniel.kjellid@gmail.com>
2023-03-24 17:42:32 +0100

    Change frontend view path

--- nest/urls.py
+++ nest/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import FrontendView
 
 urlpatterns = [
-    path("test/", FrontendView.as_view(), name="index"),
+    path("", FrontendView.as_view(), name="index"),
     path("admin/", admin.site.urls),
 ]
