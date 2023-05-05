d30f2bca9ed6c0c06362e631405cd53b5b954f24
Daniel Kjellid <daniel.kjellid@gmail.com>
2023-05-05 21:39:15 +0200

    Add endpoint tests

--- nest/products/tests/endpoints/test_product_detail.py
+++
@@ -1,6 +0,0 @@
-class TestEndpointProductDetail:
-    def test_anonymous_request_product_detail_api(self):
-        assert False
-
-    def test_staff_request_product_detail_api(self):
-        assert False
--- nest/products/tests/endpoints/test_product_edit.py
+++ nest/products/tests/endpoints/test_product_edit.py
@@ -1,24 +1,26 @@
 import pytest
-from nest.products.endpoints.product_create import product_create_api
-from nest.units.tests.utils import get_unit
+from nest.products.endpoints.product_edit import product_edit_api
 from django.test.client import MULTIPART_CONTENT
+from nest.products.tests.utils import create_product
+from nest.units.tests.utils import get_unit
 
 pytestmark = pytest.mark.django_db
 
 
-class TestEndpointProductCreate:
-    ENDPOINT = "/api/v1/products/create/"
+class TestEndpointProductEdit:
+    ENDPOINT = "/api/v1/products"
 
-    def test_anonymous_request_product_create_api(
+    def test_anonymous_request_product_edit_api(
         self, django_assert_num_queries, authenticated_client, mocker
     ):
         """
-        Test that authenticated users gets a 401 unauthorized when trying to create
+        Test that authenticated users gets a 401 unauthorized when trying to edit
         products.
         """
 
+        product = create_product()
         client = authenticated_client
-        service_mock = mocker.patch(f"{product_create_api.__module__}.create_product")
+        service_mock = mocker.patch(f"{product_edit_api.__module__}.edit_product")
 
         payload = {
             "name": "Some cool product",
@@ -27,11 +29,12 @@ class TestEndpointProductCreate:
             "unit": get_unit().id,
             "supplier": "Cool supplier",
             "is_available": True,
+            "is_synced": True,
         }
 
         with django_assert_num_queries(2):
             response = client.post(
-                self.ENDPOINT,
+                f"{self.ENDPOINT}/{product.id}/edit/",
                 data=payload,
                 content_type=MULTIPART_CONTENT,
             )
@@ -39,15 +42,16 @@ class TestEndpointProductCreate:
         assert response.status_code == 401
         assert service_mock.call_count == 0
 
-    def test_staff_request_product_create_api(
+    def test_staff_request_product_edit_api(
         self, django_assert_num_queries, authenticated_staff_client, mocker
     ):
         """
-        Test that staff users are able to create products.
+        Test that staff users get a successful response when trying to edit products.
         """
 
+        product = create_product()
         client = authenticated_staff_client
-        service_mock = mocker.patch(f"{product_create_api.__module__}.create_product")
+        service_mock = mocker.patch(f"{product_edit_api.__module__}.edit_product")
 
         payload = {
             "name": "Some cool product",
@@ -56,11 +60,12 @@ class TestEndpointProductCreate:
             "unit": get_unit().id,
             "supplier": "Cool supplier",
             "is_available": True,
+            "is_synced": True,
         }
 
         with django_assert_num_queries(2):
             response = client.post(
-                self.ENDPOINT,
+                f"{self.ENDPOINT}/{product.id}/edit/",
                 data=payload,
                 content_type=MULTIPART_CONTENT,
             )
