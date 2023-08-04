93ffedd5250319c481568c201582cf62cbd924ef
Daniel Kjellid <daniel.kjellid@gmail.com>
2023-08-04 21:26:59 +0200

    Remove unused urls

--- frontend/apps/urls.ts
+++ frontend/apps/urls.ts
@@ -5,17 +5,11 @@ const urls = {
     create: () => '/api/v1/products/create/',
     detail: ({ id }: { id: Param }) => `/api/v1/products/${id}/`,
     edit: ({ id }: { id: Param }) => `/api/v1/products/${id}/edit/`,
-    editFetch: ({ id }: { id: Param }) => `/api/v1/products/${id}/edit/`,
     list: () => '/api/v1/products/',
 
     oda: {
       import: () => '/api/v1/products/oda/import/',
       importConfirm: () => '/api/v1/products/oda/import/confirm/',
-    },
-
-    nutrition: {
-      detail: ({ id }: { id: Param }) => `/api/v1/products/${id}/nutrition/`,
-      edit: ({ id }: { id: Param }) => `/api/v1/products/${id}/nutrition/edit/`,
     },
   },
   users: {
