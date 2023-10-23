ea7d43155aac257b77a27136448fd41ddf93d4a0
Daniel Kjellid <daniel.kjellid@gmail.com>
2023-10-23 20:26:41 +0200

    Make app working again

--- nest/ingredients/selectors.py
+++ nest/ingredients/selectors.py
@@ -6,10 +6,7 @@ def get_ingredients() -> list[IngredientRecord]:
     """
     Get a list of all ingredients in the application.
     """
-    ingredients = Ingredient.objects.all().select_related("product__unit")
-    records = [
-        IngredientRecord.from_ingredient(ingredient=ingredient)
-        for ingredient in ingredients
-    ]
+    ingredients = Ingredient.objects.all().select_related("product", "product__unit")
+    records = [IngredientRecord.from_db_model(ingredient) for ingredient in ingredients]
 
     return records
