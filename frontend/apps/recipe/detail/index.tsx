3b0ab51913db460aaa6b868b5acc870f7fa57d3b
Daniel Kjellid <daniel.kjellid@gmail.com>
2023-10-26 19:43:58 +0200

    Fix status badge

--- frontend/apps/recipe/detail/index.tsx
+++ frontend/apps/recipe/detail/index.tsx
@@ -9,13 +9,14 @@ import BaseApp from '../../../components/BaseApp/BaseApp'
 import Header from '../../../components/Header'
 import { Recipe } from '../components/Recipe'
 import { Button } from '../../../components/Button'
-import { Button as MButton } from '@mantine/core'
+import { Button as MButton, Badge } from '@mantine/core'
 import { useFetcher, useNavigate, useParams } from 'react-router-dom'
 import View from '../../../components/View'
 import { RecipeDetailOut, RecipeDetailOutAPIResponse, RecipeStatus } from '../../../types'
 import { urls } from '../../urls'
 import invariant from 'tiny-invariant'
 import { useFetch } from '../../../hooks/fetcher'
+import { routes } from '../routes'
 
 interface RecipeDetailInnerProps {
   results: {
@@ -34,23 +35,40 @@ function RecipeDetailInner({ results }: RecipeDetailInnerProps) {
     navigate('/')
   }
 
+  const getRecipeBadge = (status: RecipeStatus) => {
+    const badges: Record<RecipeStatus, React.ReactNode> = {
+      1: (
+        <Badge size="lg" radius="sm" color="blue" variant="dot">
+          Draft
+        </Badge>
+      ),
+      2: (
+        <Badge size="lg" radius="sm" color="green" variant="dot">
+          Published
+        </Badge>
+      ),
+    }
+
+    return badges[status]
+  }
+
   return (
     <div className="flex flex-col space-y-6">
       {currentUser && currentUser.isStaff && (
         <div className=" flex items-center justify-between">
-          <div>Published</div>
+          {getRecipeBadge(recipe.status)}
           <div className="flex items-center space-x-3">
             {currentUser.isSuperuser && (
               <MButton
                 component="a"
-                href={'/admin/products/product/'}
+                href={`/admin/recipes/recipe/${recipe.id}/`}
                 target="_blank"
                 variant="default"
               >
                 View in admin
               </MButton>
             )}
-            <Button>Edit recipe</Button>
+            <Button onClick={() => navigate('/')}>Edit recipe</Button>
           </div>
         </div>
       )}
