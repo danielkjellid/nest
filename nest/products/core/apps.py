4db4e93822af4218eb7361695b2744f9b8b1962b
Daniel Kjellid <daniel.kjellid@gmail.com>
2023-10-31 23:00:47 +0100

    Remove oida

--- nest/products/core/apps.py
+++ nest/products/core/apps.py
@@ -4,8 +4,7 @@ from django.apps import AppConfig
 class ProductsCoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "nest.products.core"
+    label = "products"
 
     def ready(self) -> None:
         import nest.products.core.signals  # noqa
-
-    label = "products"
--- nest/products/oda/apps.py
+++ nest/products/oda/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class OdaConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "nest.products.oda"
+    label = "products_oda"
