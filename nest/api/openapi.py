2aad410969c04b5df83df5c5ef7c8a3efd5b1b31
Daniel Kjellid <daniel.kjellid@gmail.com>
2023-11-15 19:55:43 +0100

    Revert changes to openapi

--- nest/api/openapi.py
+++ nest/api/openapi.py
@@ -4,13 +4,16 @@ import inspect
 from enum import Enum
 from typing import (
     Any,
+    Collection,
     Iterable,
+    Sequence,
     Type,
     cast,
     get_args,
     get_type_hints,
 )
 
+from django.conf import settings
 from django.db.models import IntegerChoices, TextChoices
 from ninja import NinjaAPI
 from ninja.openapi.schema import (
@@ -66,6 +69,7 @@ class OpenAPISchema(NinjaOpenAPISchema):
         """
         schemas = {}
 
+        mapped_enums = self._extract_enum_from_models(models=models)
         meta = self._set_schema_meta(models=models)
 
         for key, value in schema.copy().items():
@@ -78,7 +82,9 @@ class OpenAPISchema(NinjaOpenAPISchema):
             updated_schema = {
                 schema_key: {
                     "title": schema_key,
-                    "properties": camelize(properties)
+                    "properties": self._populate_definition_properties(
+                        properties=properties, enum_mapping=mapped_enums
+                    )
                     if properties is not None
                     else None,
                     "required": self._convert_keys_to_camelcase(required),
@@ -168,8 +174,7 @@ class OpenAPISchema(NinjaOpenAPISchema):
 
             self.schemas.update({m_schema["title"]: m_schema})
 
-    @staticmethod
-    def enum_process_schema(enum: Type[IntegerChoices]) -> dict[str, Any]:
+    def enum_process_schema(self, enum: Type[IntegerChoices]) -> dict[str, Any]:
         """
         Process enum and create a dict of openapi spec.
         """
@@ -189,6 +194,113 @@ class OpenAPISchema(NinjaOpenAPISchema):
         }
 
         return schema_
+
+    ###########
+    # Helpers #
+    ###########
+
+    @staticmethod
+    def _format_enum_from_type(typ: Any) -> list[dict[str, str | int]] | None:
+        """
+        Format schema field's enum type into a key - value format, taking advantage
+        of Django's human-readable labels where applicable.
+        """
+
+        type_to_check = typ
+        args_iterable = get_args(typ)
+
+        if args_iterable:
+            type_to_check = next(
+                (
+                    item
+                    for item in args_iterable
+                    if inspect.isclass(item) and issubclass(item, Enum)
+                ),
+                None,
+            )
+
+            if not type_to_check:
+                return None
+
+        # If passed enum is a django choices field, we can take advantaged
+        # of the defined label.
+        if issubclass(type_to_check, IntegerChoices | TextChoices):
+            return [
+                {"label": item.label, "value": item.value} for item in type_to_check
+            ]
+        elif issubclass(type_to_check, Enum):
+            return [
+                {"label": item.name.replace("_", " ").title(), "value": item.value}
+                for item in type_to_check
+            ]
+
+        return None
+
+    def _extract_enum_from_models(
+        self, models: TModels[Any]
+    ) -> list[dict[str, Sequence[Collection[str]]]]:
+        """
+        Iterate through the models passed by the operation and return the field as well
+        as enum representation.
+        """
+        enum_mapping = []
+
+        for model in models:
+            for _key, val in model.__fields__.items():
+                for field_name, type_ in get_type_hints(val.type_).items():
+                    enum = self._format_enum_from_type(typ=type_)
+
+                    if enum:
+                        enum_mapping.append(
+                            {
+                                "field": field_name,
+                                "enum": enum,
+                            }
+                        )
+
+        return enum_mapping
+
+    def _populate_definition_properties(
+        self,
+        properties: dict[str, Any],
+        enum_mapping: list[dict[str, Sequence[Collection[str]]]] | None = None,
+    ) -> dict[str, Any]:
+        """
+        This method does a few things to modify the definitions dict in a way we want
+        it. It first converts the keys to camelcase. Then it proceeds to iterate through
+        the values and append an enum key with a list of enum labels and values if one
+        of the previous extracted enum mapping matches the iterated key. Then we set
+        the correct component for the python type.
+
+        """
+        props = self._convert_keys_to_camelcase(properties.copy())
+
+        for property, property_value in props.items():
+            property_type = property_value.get("type", None)
+            for _key, _value in property_value.copy().items():
+                if enum_mapping:
+                    for mapping in enum_mapping:
+                        if mapping["field"] == property:
+                            props[property]["title"] = mapping["field"].title()  # type: ignore
+                            props[property]["enum"] = mapping["enum"]
+
+                            component_defaults = (
+                                settings.FORM_COMPONENT_MAPPING_DEFAULTS  # type: ignore
+                            )
+                            component = component_defaults["enum"].value
+                            props[property]["component"] = component
+
+                if property_type and props[property].get("component", None) is None:
+                    try:
+                        props[property][
+                            "component"
+                        ] = settings.FORM_COMPONENT_MAPPING_DEFAULTS[  # type: ignore
+                            property_value["type"]
+                        ].value
+                    except KeyError:
+                        pass
+
+        return camelize(props)  # type: ignore
 
     def _convert_keys_to_camelcase(self, data: dict[str, Any] | list[str]) -> Any:
         """
@@ -231,5 +343,12 @@ class OpenAPISchema(NinjaOpenAPISchema):
                     continue
 
                 meta["title"] = f"{val.__name__}"
+
+                if hasattr(val, "FormMeta"):
+                    meta["columns"] = getattr(
+                        value.FormMeta,
+                        "columns",
+                        1,  # type: ignore
+                    )
 
         return meta
