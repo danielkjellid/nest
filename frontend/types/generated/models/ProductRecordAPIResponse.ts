09302159c07e2cc9818bfe8bde8afd3086233088
Daniel Kjellid <daniel.kjellid@gmail.com>
2023-12-20 23:29:56 +0100

    Update types

---
+++ frontend/types/generated/models/ProductRecordAPIResponse.ts
@@ -0,0 +1,23 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { ProductRecord } from './ProductRecord';
+
+export type ProductRecordAPIResponse = {
+    status: ProductRecordAPIResponse.status;
+    message?: string;
+    data?: ProductRecord;
+};
+
+export namespace ProductRecordAPIResponse {
+
+    export enum status {
+        SUCCESS = 'success',
+        ERROR = 'error',
+    }
+
+
+}
+
---
+++ frontend/types/generated/models/ProductRecordListAPIResponse.ts
@@ -0,0 +1,23 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { ProductRecord } from './ProductRecord';
+
+export type ProductRecordListAPIResponse = {
+    status: ProductRecordListAPIResponse.status;
+    message?: string;
+    data?: Array<ProductRecord>;
+};
+
+export namespace ProductRecordListAPIResponse {
+
+    export enum status {
+        SUCCESS = 'success',
+        ERROR = 'error',
+    }
+
+
+}
+
